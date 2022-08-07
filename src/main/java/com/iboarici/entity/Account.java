edf733edbdee92277912b7b3984f3da57951e80e
iboarici <iboarici@gmail.com>
2022-08-07 21:36:12 +0200

    Account development

---
+++ src/main/java/com/iboarici/entity/Account.java
@@ -0,0 +1,23 @@
+package com.iboarici.entity;
+
+public class Account {
+
+    private String accountType;
+    private Double balance;
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
